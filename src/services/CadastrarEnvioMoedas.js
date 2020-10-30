422aca670e9fb7c5e99c3089f5e79de76e074619
MaxwelRamos <gronesoft.atendimento@gmail.com>
2020-10-30 20:08:20 -0300

    teste comunicacao heroku

--- src/services/CadastrarEnvioMoedas.js
+++ src/services/CadastrarEnvioMoedas.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { toastr } from 'react-redux-toastr'
 
-const BASE_URL = 'http://localhost:8080'
+// const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://b-meriymoney.herokuapp.com'
 
 export class CadastrarEnvioMoedas {
 
     static createEnvioMoeda(values) {
+        console.log(BASE_URL)
         return axios.post(`${BASE_URL}/moedas`, values)
             .then((response) => {
                 toastr.success('Sucesso', response.data.message)
